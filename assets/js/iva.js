fddd7dc4e1302e74986609428f46bdf110676cc0
pablo iturra <pabloiturra@siamec.cl>
2022-03-25 17:36:36 -0300

    commit

---
+++ assets/js/iva.js
@@ -0,0 +1,7 @@
+if(document.getElementById("btnModal")){
+    var nombre = document.getElementById('Nombre').value;
+    var apellido = document.getElementById('Apellido').value;
+    var lavado = document.getElementById('lavado').value;
+    var motor = document.getElementById('motor').value;
+    
+}
